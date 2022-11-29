91996492d9c720371a27a88cbbd2b7967129b7ca
thomas0913 <89578577+thomas0913@users.noreply.github.com>
2022-11-29 17:49:51 +0800

    complete simple_switch_13.py comment descriptions

--- SDN/sdn-controller/simple_switch_13.py
+++ SDN/sdn-controller/simple_switch_13.py
@@ -1,5 +1,5 @@
 # ============================================
-# ||        基本Ryu Switch Conreoller       ||
+# ||        基本Ryu Switch Controller       ||
 # ============================================
 
 # 匯入Ryu基本組件模組 | Ryu app核心管理組件
@@ -30,7 +30,9 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
         # 基於ARP協議，紀錄學習實體位址(Mac Address)與端口(port)之對映關係表
         self.mac_to_port = {}
     
-    # 引入事件監聽器，
+    # ======================== 交握連接處理 ===========================
+    # 引入事件監聽器，並匯入Switch功能的OpenFlow事件類實例，以及指定交握階段為CONFIG_DISPATCHER生成於handler中
+    # CONFIG_DISPATCHER : Version negotiated and sent features-request message
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         '''
@@ -85,6 +87,8 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
         # Controller向Switch下達新的流表訊息
         datapath.send_msg(mod)
 
+    # ======================== 數據傳送處理 ===========================
+    # MAIN_DISPATCHER : Switch-features message received and sent set-config message
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         '''
@@ -92,21 +96,32 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
                 1. 透過更換傳入封包中的來源、目的IP地址來實現RHM演算法。
                 2. 處理傳入的封包。
         '''
+        # 代表 Packet-In 傳入的資訊（包含 switch、in_port_number 等...）
         msg = ev.msg
+        # 在 OpenFlow 中，datapath 代表的就是 Switch
         datapath = msg.datapath
+        # 取出此 Switch 中，使用的 OpenFlow 協定
         ofproto = datapath.ofproto
+        # 取出此 Switch 中，使用的Switch 與 Ryu 之間的溝通管道
         parser = datapath.ofproto_parser
+        # get the received port number from packet_in message.
         in_port = msg.match['in_port']
 
+        # 解析獲取信息中所包含的封包資料
         pkt = packet.Packet(msg.data)
+        # 獲取第一個找到的與以太網路協議匹配的協議。
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
+        # ignore lldp packet(設備資訊封包)
+        if eth.ethertype == packet.ether_types.ETH_TYPE_LLDP:
             return
+        
+        # 封包目的Mac地址
         dst = eth.dst
+        # 封包來源Mac地址
         src = eth.src
 
+        # Switch編號
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
         print("------------------------------------------")
@@ -115,10 +130,9 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
         print("..")
         print("..")
 
-        #self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-        print("==================================")
+        print("=================================")
         print("|        Packet In Event        |")
-        print("==================================")
+        print("=================================")
         print("dpid => ", dpid)
         print("src => ", src)
         print("dst => ", dst)
@@ -139,22 +153,25 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
         print("..")
         print("..")
 
+        
         if dst in self.mac_to_port[dpid]:
+            # 當MAC地址表中存在目的MAC地址時，使用相應的端口號
             out_port = self.mac_to_port[dpid][dst]
         else:
+            # 否則將指定OFPP_FLOOD端口，傳往除了 in_port 外的所有 port 上
             out_port = ofproto.OFPP_FLOOD
 
+        # construct action list.
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
+            # verify if we have a valid buffer_id, if yes avoid to send both flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                print("======================")
-                print("|    Add Flow Entry  |")
-                print("======================")
+                print("========================")
+                print("|    Add Flow Entry    |")
+                print("========================")
                 print(datapath)
                 print(1)
                 print(match)
@@ -166,9 +183,9 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
                 self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                 return
             else:
-                print("======================")
-                print("|    Add Flow Entry  |")
-                print("======================")
+                print("========================")
+                print("|    Add Flow Entry    |")
+                print("========================")
                 print(datapath)
                 print(1)
                 print(match)
@@ -177,10 +194,12 @@ class RandomHostMutation_Switch(app_manager.RyuApp):
                 print("..")
                 print("..")
                 self.add_flow(datapath, 1, match, actions)
+
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
+        # construct packet_out message and send it.
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
